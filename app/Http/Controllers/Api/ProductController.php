8efff07772f9bb1ef93ead6add367e56f56bd9dd
kishan-gk <147246588+kishan-gk@users.noreply.github.com>
2025-03-28 01:29:48 +0530

    Add files via upload

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+   public function index(){
+    return response()->json(Product::orderBy('created_at','DESC')->get());
+   }
+}
